f2c60d30a963272c9874a48bb6845ea0da9e5039
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2023-07-08 03:44:59 -0700

    implement trace logging level, logging randomGenerator

--- main.py
+++ main.py
@@ -70,24 +70,87 @@ class App(tk.Tk):
         self.mainloop()
 
     def initLogging(self):
-        self.loglevel = logging.DEBUG
         self.loglevelReference = {
             0 :"NOTSET",
+            5 :"TRACE",
             10:"DEBUG",
             20:"INFO",
             30:"WARNING",
             40:"ERROR",
             50:"CRITICAL"
         }
+        self.addLoggingLevel('TRACE', logging.DEBUG - 5)
+        self.logLevel = logging.TRACE
+        # self.logLevel = logging.DEBUG
+        # self.logLevel = logging.INFO
+        # self.logLevel = logging.WARNING
+        # self.logLevel = logging.ERROR
+        # self.logLevel = logging.CRITICAL
+
         self.programLogger = logging.basicConfig(filename='example.log', 
             encoding='utf-8', 
-            level=self.loglevel, 
+            level=self.logLevel, 
             format='[{asctime}.{msecs:0<3.0f}][{levelname:^8s}] Module \'{module}\': {message}',
             style='{',
             filemode='w',
             datefmt='%X')
         logging.info("Robot Warehousing Simulator program started.")
-        logging.info(f"Logging started with level: {self.loglevelReference[self.loglevel]}")
+        logging.info(f"Logging started with level: {self.loglevelReference[self.logLevel]}")
+
+        # Quick and dirty workaround for correctly logging {module} as the invoking module
+        # Before, when in logging_extensions.py, the module would be logging_extensions
+    
+    def addLoggingLevel(self, levelName, levelNum, methodName=None):
+            """
+            https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
+            Comprehensively adds a new logging level to the `logging` module and the
+            currently configured logging class.
+
+            `levelName` becomes an attribute of the `logging` module with the value
+            `levelNum`. `methodName` becomes a convenience method for both `logging`
+            itself and the class returned by `logging.getLoggerClass()` (usually just
+            `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+            used.
+
+            To avoid accidental clobberings of existing attributes, this method will
+            raise an `AttributeError` if the level name is already an attribute of the
+            `logging` module or if the method name is already present 
+
+            Example
+            -------
+            >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+            >>> logging.getLogger(__name__).setLevel("TRACE")
+            >>> logging.getLogger(__name__).trace('that worked')
+            >>> logging.trace('so did this')
+            >>> logging.TRACE
+            5
+
+            """
+            if not methodName:
+                methodName = levelName.lower()
+
+            if hasattr(logging, levelName):
+                raise AttributeError('{} already defined in logging module'.format(levelName))
+            if hasattr(logging, methodName):
+                raise AttributeError('{} already defined in logging module'.format(methodName))
+            if hasattr(logging.getLoggerClass(), methodName):
+                raise AttributeError('{} already defined in logger class'.format(methodName))
+
+            # This method was inspired by the answers to Stack Overflow post
+            # http://stackoverflow.com/q/2183233/2988730, especially
+            # http://stackoverflow.com/a/13638084/2988730
+            def logForLevel(self, message, *args, **kwargs):
+                print("Log for level")
+                if self.isEnabledFor(levelNum):
+                    self._log(levelNum, message, args, **kwargs)
+            def logToRoot(message, *args, **kwargs):
+                print("Log to root")
+                logging.log(levelNum, message, *args, **kwargs)
+
+            logging.addLevelName(levelNum, levelName)
+            setattr(logging, levelName, levelNum)
+            setattr(logging.getLoggerClass(), methodName, logForLevel)
+            setattr(logging, methodName, logToRoot)
 
     def simulationConfiguration(self):
         self.simulationConfigWindow = simulationConfigManager(self)
--- modules/randomGenerator.py
+++ modules/randomGenerator.py
@@ -1,4 +1,5 @@
 import random
+import logging
 
 class randomGenerator:
     """
@@ -8,27 +9,39 @@ class randomGenerator:
         self.parent = parent
         self.randomGeneratorState = randomGeneratorState(self)
         random.seed(self.randomGeneratorState.currentSeed)
+        logging.debug(f"Random generator engine seeded with {self.randomGeneratorState.currentSeed}.")
+        logging.info("Class 'randomGenerator' initialized.")
 
     def randomChoice(self, sequence):
         # Make a choice out of a sequence at random using the seeded generator
         # Be aware that when selecting a random node from a nodeView, the returned value is a string of the node name, not a tuple
+        logging.debug(f"Request for choice from: {sequence}")
         choice = random.choice(sequence)
+        logging.info("Random selection made.")
+        logging.debug(f"Result: {choice}")
         return choice
 
     def updateCurrentSeed(self, value):
         # Update generator's seed
+        logging.debug(f"Request received to update the seed value for 'randomGenerator' to: {value}")
         random.seed(value)
         self.randomGeneratorState.currentSeed = value
+        logging.info("'randomGenerator' seed value updated.")
+        logging.debug(f"New value: {self.randomGeneratorState.currentSeed}")
 
     def packageRandomGeneratorData(self):
         """
             Package data for replicating the current session
                 - currentSeed
         """
+        logging.info("Received request to package 'randomGenerator' data.")
         dataPackage = {}
         dataPackage["currentSeed"] = self.randomGeneratorState.currentSeed
+        logging.debug(f"Packaged agentData: {dataPackage}")
+        logging.info("Packaged all 'randomGenerator' data")
         return dataPackage
 
 class randomGeneratorState:
     def __init__(self, parent):
         self.currentSeed = 123456789 # Default value
+        logging.info(f"Class 'randomGeneratorState' initialized, with default seed {self.currentSeed}.")
