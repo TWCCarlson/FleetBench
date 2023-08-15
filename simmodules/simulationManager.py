ad6e41149f52435d8280339f8f5ad9b9bab79609
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2023-08-15 15:23:58 -0700

    frame management and task frequency options

--- simmodules/simulationManager.py
+++ simmodules/simulationManager.py
@@ -44,11 +44,11 @@ class simulationConfigManager(tk.Toplevel):
 
     def buildPathfindingAlgorithmPage(self):
         # Intermediate function grouping together declarations and renders for the algorithm choices page
-        self.createAlgorithmChoices()
+        self.createAlgorithmOptions()
 
     def buildTaskGenerationPage(self):
         # Intermediate function grouping together declarations and renders for the task generation page
-        pass
+        self.createTaskFrequencyOptions()
 
     def buildDisplayOptionsPage(self):
         # Intermediate function grouping together declarations and renders for the display options page
@@ -63,12 +63,13 @@ class simulationConfigManager(tk.Toplevel):
             Packages the current simulation configuration for saving
         """
         dataPackage = {}
-        dataPackage["algorithmSelection"] = self.algorithmChoiceState
+        dataPackage["algorithmSelection"] = self.algorithmSelectionStringVar.get()
         dataPackage["playbackFrameDelay"] = self.frameDelayValue.get()
+        dataPackage["taskGenerationFrequencyMethod"] = self.taskFrequencySelectionStringvar.get()
 
         return dataPackage
     
-    def createAlgorithmChoices(self):
+    def createAlgorithmOptions(self):
         # Creates a drop down menu for the user to select the driving algorithm for the simulation
         # Using types to separate multi-agent and single-agent pathfinding algorithms
         optionTypeDict = {
@@ -77,17 +78,16 @@ class simulationConfigManager(tk.Toplevel):
         }
 
         # Keys of the dict are the displayed options
-        options = list(optionTypeDict.keys())
+        algorithmOptions = list(optionTypeDict.keys())
 
         # Stringvar to hold the algorithm selection
         self.algorithmSelectionStringVar = tk.StringVar()
 
         # Set a default selection - maybe skip this to force a choice
-        self.algorithmChoiceState = self.algorithmSelectionStringVar
-        self.algorithmChoiceState.set(options[0])
+        self.algorithmSelectionStringVar.set(algorithmOptions[0])
 
         # Declare the drop down menu
-        self.algorithmChoiceMenu = tk.OptionMenu(self.pathfindingAlgorithmFrame, self.algorithmChoiceState, *options)
+        self.algorithmChoiceMenu = tk.OptionMenu(self.pathfindingAlgorithmFrame, self.algorithmSelectionStringVar, *algorithmOptions)
 
         # If there is more than one agent in the simulation space, disable single-agent pathfinding algorithm options
         for algorithm, type in optionTypeDict.items():
@@ -96,6 +96,99 @@ class simulationConfigManager(tk.Toplevel):
 
         # Render the menu
         self.algorithmChoiceMenu.grid(row=1, column=0)
+
+    def createTaskFrequencyOptions(self):
+        # Create a containing frame for this section
+        # Useful for deleting all containing widgets later
+        self.taskFrequencyChoicesFrame = tk.Frame(self.taskGenerationFrame)
+
+        # Creates a drop-down for task frequency type
+        # Based on selection, creates options specific to the frequency type
+        self.taskFrequencyMethodOptionsLabel = tk.Label(self.taskFrequencyChoicesFrame, text="Task Gen. Freq.:")
+
+        # Dict pairing options and respective ui creation functions
+        taskFrequencyMethodOptionsDict = {
+            "Fixed Rate": self.buildFixedRateTaskGenerationOptions,
+            "As Available": self.buildAsAvailableTaskGenerationOptions
+        }
+
+        # Menu options are the text in the dict keys
+        taskFrequencyMethodMenuOptions = list(taskFrequencyMethodOptionsDict.keys())
+
+        # Stringvar to hold the option selection
+        self.taskFrequencySelectionStringvar = tk.StringVar()
+
+        # Add a trace to the stringvar to call the associated UI function when the user makes a selection from the menu
+        self.taskFrequencySelectionStringvar.trace_add("write", lambda *args, selection=self.taskFrequencySelectionStringvar, parentFrame=self.taskFrequencyChoicesFrame: taskFrequencyMethodOptionsDict[selection.get()](self.taskFrequencyChoicesFrame))
+
+        # Declare the dropdown menu, linking it to the stringvar
+        self.taskFrequencyMethodMenu = tk.OptionMenu(self.taskFrequencyChoicesFrame, self.taskFrequencySelectionStringvar, *taskFrequencyMethodMenuOptions)
+        # Render the menu and its label
+        self.taskFrequencyChoicesFrame.grid(row=0, column=0)
+        self.taskFrequencyMethodOptionsLabel.grid(row=0, column=0)
+        self.taskFrequencyMethodMenu.grid(row=0, column=1)
+        
+        # Set a default selection - maybe skip this to force a choice
+        self.taskFrequencySelectionStringvar.set(taskFrequencyMethodMenuOptions[0])
+
+    def buildFixedRateTaskGenerationOptions(self, parentFrame):
+        # Clear the parent frame of any leftovers to make room for this frame and its children
+        self.removeSubframes(parentFrame)
+
+        # Create a containing frame for this section
+        # Useful for deleting all containing widgets later
+        self.taskFixedRateOptionsFrame = tk.Frame(parentFrame)
+
+        # Creates UI elements relevant to setting options for task generation at fixed rates
+        self.taskFixedRateMethodOptionsLabel = tk.Label(self.taskFixedRateOptionsFrame, text="Rate setting:")
+
+        # Dict pairing options and respective ui creation functions
+        taskFixedRateMethodOptionsDict = {
+            "Custom": self.buildFixedRateTaskCustomRate,
+            "Auto: Mean Task Length": self.buildFixedRateTaskMeanRate,
+            "Auto: Max Task Length": self.buildFixedRateTaskMaxRate,
+            "Auto: Min Task Length": self.buildFixedRateTaskMinRate,
+            "Auto: Median Task Length": self.buildFixedRateTaskMedianRate
+        }
+
+        # Menu options are the text in the dict keys
+        taskFixedRateMethodOptions = list(taskFixedRateMethodOptionsDict.keys())
+
+        # Stringvar to hold the option selection
+        self.taskFixedRateSelectionStringvar = tk.StringVar()
+
+        # Add a trace to the stringvar to call the associated UI function when the user makes a selection from the menu
+        self.taskFixedRateSelectionStringvar.trace_add("write", lambda *args, selection=self.taskFixedRateSelectionStringvar: taskFixedRateMethodOptionsDict[selection.get()]())
+
+        # Set a default selection - maybe skip this to force a choice
+        self.taskFixedRateSelectionStringvar.set(taskFixedRateMethodOptions[0])
+
+        # Declare the dropdown menu, linking it to the stringvar
+        self.taskFixedRateMethodMenu = tk.OptionMenu(self.taskFixedRateOptionsFrame, self.taskFixedRateSelectionStringvar, *taskFixedRateMethodOptions)
+
+        # Render the menu and its label
+        self.taskFixedRateOptionsFrame.grid(row=0, column=2)
+        self.taskFixedRateMethodOptionsLabel.grid(row=0, column=0)
+        self.taskFixedRateMethodMenu.grid(row=0, column=1)
+    
+    def buildFixedRateTaskCustomRate(self):
+        pass
+
+    def buildFixedRateTaskMeanRate(self):
+        pass
+
+    def buildFixedRateTaskMaxRate(self):
+        pass
+
+    def buildFixedRateTaskMinRate(self):
+        pass
+
+    def buildFixedRateTaskMedianRate(self):
+        pass
+
+    def buildAsAvailableTaskGenerationOptions(self, parentFrame):
+        # Clear the parent frame of any leftovers to make room for this frame and its children
+        self.removeSubframes(parentFrame)
 
     def createSimulationUpdateRate(self):
         self.frameDelayLabel = tk.Label(self.displayOptionsFrame, text="frameDelay:")
@@ -149,6 +242,18 @@ class simulationConfigManager(tk.Toplevel):
             # self.validFrameDelayEntry = False
             logging.debug("Playback frame delay entry is non-numeric and invalid.")
             return False
+
+    def removeSubframes(self, frame):
+        # Destroy all widgets inside child frames of the passed frames, and the child frames themselves
+        # Find all child widgets
+        for widget in frame.winfo_children():
+            # Identify which ones are frames
+            if isinstance(widget, tk.Frame):
+                # Destroy the frame—all children are also automatically destroyed
+                # https://www.tcl.tk/man/tcl8.6/TkCmd/destroy.html
+                # "This command deletes the windows given by the window arguments, plus all of their descendants."
+                print(f"widget {widget} destroyed")
+                widget.destroy()
 
 class simulationConfigurationState:
     # Holds the current state of the simulation config
